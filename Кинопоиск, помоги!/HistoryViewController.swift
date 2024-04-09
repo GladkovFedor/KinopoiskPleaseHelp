c83c7d8000ee2e03856bbaa70340a2dbb7d4f25f
Федор Гладков <fedor.9619@gmail.com>
2024-04-09 19:56:06 +0700

    КПП-1.0 - Main.storybaord was removed. A tabbar with icons from SF-Symbols was added as rootViewController in SceneDelegate.

---
+++ Кинопоиск, помоги!/HistoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryViewController.swift
+//  Кинопоиск, помоги!
+//
+//  Created by Федор Гладков on 09.04.2024.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
